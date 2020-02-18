17817babd36c1d9b44a933e26cace03db01ff86a
Salvador <salvador.on@outlook.com>
2020-02-18 18:52:50 -0300

    add inputs to validate the objects and functions

--- bin/main.rb
+++ bin/main.rb
@@ -1,32 +1,27 @@
 class Game
-    attr_accessor :grid, :player1, :player2
+  attr_accessor :grid, :player1, :player2
+  def initialize(player1, player2, grid)
+    @player1 = player1
+    @player2 = player2
+    @grid = grid
+  end
 
-    def initialize(player1, player2, grid)
-        @player1 = player1
-        @player2 = player2
-        @grid = grid
-    end
-
-
-    def movements (player, num)
-        @grid.grid[num] = player.symbol
-    end
-    
-
-
+  def movements (player, num)
+    @grid.grid[num - 1] = player.symbol
+  end
 end
 
-    class Grid
-        attr_accessor :grid
-        def initialize()
-            @grid = [1, 2, 3 , 4, 5, 6, 7, 8, 9]
-        end
-
-        def board
-            res = " #{ @grid [0]} | #{ @grid [1]} | #{ @grid [2]}\n-----------\n #{ @grid [3]} | #{ @grid [4]} | #{ @grid [5]}\n-----------\n #{ @grid [6]} | #{ @grid [7]} | #{ @grid [8]}"
-            return res
-        end
+class Grid
+  attr_accessor :grid
+  def initialize()
+    @grid = [1, 2, 3 , 4, 5, 6, 7, 8, 9]
     end
+
+  def board
+    res = " #{ @grid [0]} | #{ @grid [1]} | #{ @grid [2]}\n-----------\n #{ @grid [3]} | #{ @grid [4]} | #{ @grid [5]}\n-----------\n #{ @grid [6]} | #{ @grid [7]} | #{ @grid [8]}"
+      return res
+      end
+  end
 
     class Players
         attr_accessor :name, :symbol
@@ -40,11 +35,27 @@ end
 
 
 grid = Grid.new()
-p1 = Players.new('salvador', 'X')
-p2 = Players.new('daniel','o')
+puts 'Enter player 1 name'
+name=gets.chomp
+puts 'What do you want to use X or O'
+xo=gets.chomp
+p1 = Players.new(name, xo)
+if xo == 'x' || xo == 'X' 
+  xo = 'O'
+else 
+  xo = 'x'
+end
+puts 'Enter player 2 name'
+name=gets.chomp
+p2 = Players.new(name, xo)
 game = Game.new(p1, p2, grid)
-game.movements(p1, 3)
 puts grid.board
-puts p1.name
-puts p2.name
-puts game.grid.board
+puts 'Player 1 choose your position'
+pos=gets.chomp
+game.movements(p1, pos.to_i)
+puts 'Player 2 choose your position'
+puts grid.board
+pos=gets.chomp
+game.movements(p2, pos.to_i)
+puts grid.board
+
