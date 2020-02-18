9eed85ea36913fe9b2103c9874966669ce21f053
Dandush03 <d.laloush@outlook.com>
2020-02-18 18:22:55 -0400

    Fixed Some Rubocop Errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Class Game For Initialization of the Game
 class Game
   attr_accessor :grid, :player1, :player2
   def initialize(player1, player2, grid)
@@ -6,56 +9,57 @@ class Game
     @grid = grid
   end
 
-  def movements (player, num)
+  def movements(player, num)
     @grid.grid[num - 1] = player.symbol
   end
 end
 
+# Grid Class Create the Map of the Game
 class Grid
   attr_accessor :grid
-  def initialize()
-    @grid = [1, 2, 3 , 4, 5, 6, 7, 8, 9]
-    end
-
-  def board
-    res = " #{ @grid [0]} | #{ @grid [1]} | #{ @grid [2]}\n-----------\n #{ @grid [3]} | #{ @grid [4]} | #{ @grid [5]}\n-----------\n #{ @grid [6]} | #{ @grid [7]} | #{ @grid [8]}"
-      return res
-      end
+  def initialize
+    @grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-    class Players
-        attr_accessor :name, :symbol
-        def initialize(name,symbol)
-            @name = name.capitalize
-            @symbol = symbol.capitalize
-        end
-    end
-
-    
-
-
-grid = Grid.new()
-puts 'Enter player 1 name'
-name=gets.chomp
-puts 'What do you want to use X or O'
-xo=gets.chomp
-p1 = Players.new(name, xo)
-if xo == 'x' || xo == 'X' 
-  xo = 'O'
-else 
-  xo = 'x'
+  def board
+    res = " #{@grid [0]} | #{@grid [1]} | #{@grid [2]}\n-----------\n "
+    res += "#{@grid [3]} | #{@grid [4]} | #{@grid [5]}\n-----------\n "
+    res += "#{@grid [6]} | #{@grid [7]} | #{@grid [8]}"
+    res
+  end
 end
+
+# Player Init a Player
+class Players
+  attr_accessor :name, :symbol
+  def initialize(name, symbol)
+    @name = name.capitalize
+    @symbol = symbol.capitalize
+  end
+end
+
+grid = Grid.new
+puts 'Enter player 1 name'
+name = gets.chomp
+puts 'What do you want to use X or O'
+xo = gets.chomp
+xo = xo.upcase
+p1 = Players.new(name, xo)
+ox = if xo == 'X'
+       'O'
+     else
+       'x'
+     end
 puts 'Enter player 2 name'
-name=gets.chomp
-p2 = Players.new(name, xo)
+name = gets.chomp
+p2 = Players.new(name, ox)
 game = Game.new(p1, p2, grid)
 puts grid.board
 puts 'Player 1 choose your position'
-pos=gets.chomp
+pos = gets.chomp
 game.movements(p1, pos.to_i)
 puts 'Player 2 choose your position'
 puts grid.board
-pos=gets.chomp
+pos = gets.chomp
 game.movements(p2, pos.to_i)
 puts grid.board
-
