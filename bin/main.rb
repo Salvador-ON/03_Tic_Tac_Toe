42e0491960f79920de6239c57a7ba612b2c6d192
Dandush03 <d.laloush@outlook.com>
2020-02-19 15:49:39 -0400

    Tried to conv array to 2 diemnsional array

--- bin/main.rb
+++ bin/main.rb
@@ -2,11 +2,12 @@
 
 # Class Game For Initialization of the Game
 class Game
-  attr_accessor :grid, :player1, :player2
+  attr_accessor :grid, :player1, :player2, :array_res
   def initialize(player1, player2, grid)
     @player1 = player1
     @player2 = player2
     @grid = grid
+    @a_res = [[1,2,3],[1,2,3]]
   end
 
   def movements(player, num)
@@ -62,42 +63,81 @@ class Validation
            end
     bool
   end
+
   def inputs(input, grid)
     input = input.to_i
     bool = if (input <= 9) && (input > 0)
              bool = if (grid.grid[input - 1].is_a?(Numeric))
-                      bool = true
+                      true
                     else
-                      bool = false
+                      false
                     end
            else 
-             bool = false
+             false
            end
     bool
   end
+
   def names(name)
     name.strip.empty? ? bool = true : bool = false
     bool
   end
+
   def symbols(symbol)
-    array = ['X', 'O']
+    array = %w[X O]
     array.any?(symbol) ? bool = false : bool = true
     bool
   end
 end
 
-def turn(player, game, grid, validator)
-  who_play = game.who_plays(player)
-  puts "#{who_play.name} make your move, in a available numeric space"
-  puts grid.board
-  pos = gets.chomp
-  validate_input = validator.inputs(pos, grid)
-  while  !validate_input
+def validate_input(pos, grid, validator)
+  validating = validator.inputs(pos, grid)
+  until validating
     puts 'Invalid postion please reenter a correct position'
     pos = gets.chomp
-    validate_input = validator.inputs(pos, grid)
+    validating = validator.inputs(pos, grid)
   end
+end
+
+def conv_to_array(game, grid)
+  k = 0
+  t = 0
+  temp = game.grid.grid[1]
+  puts temp
+  game.grid.grid.each do |n|
+    if t == 0
+      puts t
+      puts game.a_res.inspect
+      game.a_res[t][k] = n
+      k += 1
+      if k == 2 
+        k = 0
+        t += 1
+      end
+    elsif t == 1
+      game.a_res[t][k] = n
+      k += 1
+      if k == 2 
+        k = 0
+        t += 1
+      end
+    else
+      game.a_res[t][k] = n
+      k += 1
+      if k == 2 
+        k = 0
+        t += 1
+      end
+    end
+  end
+end
+
+def turn(player, game, grid, validator)
+  who_play = game.who_plays(player)
+  pos = gets.chomp
+  validate_input(pos, grid, validator)
   game.movements(who_play, pos.to_i)
+  conv_to_array(game, grid)
   who_play
 end
 
@@ -137,21 +177,11 @@ while temp_bool
 end
 p2 = Players.new(name, ox)
 game = Game.new(p1, p2, grid)
-puts grid.board
 temp_game = game.playing?
-puts "#{p1.name} make your firts move"
-pos = gets.chomp
-validate_input = validator.inputs(pos, grid)
-while  !validate_input
-  puts 'Invalid postion please reenter a correct position'
-  pos = gets.chomp
-  validate_input = validator.inputs(pos, grid)
-end
 who_play = game.who_plays(p2)
-game.movements(p1, pos.to_i)
 while temp_game
+  puts "#{who_play.name} make your move, in a available numeric space"
+  puts grid.board
   who_play = turn(who_play, game, grid, validator)
   temp_game = game.playing?
 end
-
-
