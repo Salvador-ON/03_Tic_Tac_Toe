808984e246d241b9749e1d5478ae6915087d76be
Salvador <salvador.on@outlook.com>
2020-02-19 14:18:39 -0300

    validation input

--- bin/main.rb
+++ bin/main.rb
@@ -54,7 +54,7 @@ end
 
 # Validation of Variables
 class Validation
-  def move(input,grid)
+  def move(input, grid)
     bool = if grid.grid[input].is_a?(Numeric)
              true
            else
@@ -62,12 +62,17 @@ class Validation
            end
     bool
   end
-  def inputs(input)
-    bool = if (input <= 9) && (input > 0) && !(input.any?(' '))
-             bool = true
-           else
-             bool = false
-           end
+  def inputs(input, grid)
+    input = input.to_i
+    bool = if (input <= 9) && (input > 0)
+             bool = if !(grid.grid[input].is_a?(Numeric))
+            bool = true
+             else
+              bool = false
+            end
+        else 
+          bool = false
+        end
     bool
   end
   def names(name)
@@ -76,16 +81,22 @@ class Validation
   end
   def symbols(symbol)
     array = ['X', 'O']
-    symbol.any?(array) ? bool = true : bool = false
+    array.any?(symbol) ? bool = false : bool = true
     bool
   end
 end
 
-def turn(player,game,grid)
+def turn(player, game, grid, validator)
   who_play = game.who_plays(player)
   puts "#{who_play.name} make your move, in a available numeric space"
   puts grid.board
   pos = gets.chomp
+  validate_input = validator.inputs(pos, grid)
+  while  !validate_input
+  puts 'Invalid postion please reenter a correct position'
+  pos = gets.chomp
+  validate_input = validator.inputs(pos, grid)
+  end
   game.movements(who_play, pos.to_i)
   who_play
 end
@@ -103,6 +114,13 @@ end
 puts 'What do you want to use X or O'
 xo = gets.chomp
 xo = xo.upcase
+validate_symbol = validator.symbols(xo)
+while  validate_symbol
+  puts 'Invalid value please reenter X or O'
+  xo = gets.chomp
+  xo = xo.upcase
+  validate_symbol = validator.symbols(xo)
+end
 p1 = Players.new(name, xo)
 ox = if xo == 'X'
        'O'
@@ -111,16 +129,28 @@ ox = if xo == 'X'
      end
 puts 'Enter player 2 name'
 name = gets.chomp
+temp_bool = validator.names(name)
+while temp_bool
+  puts 'Invalid value please reenter a name'
+  name = gets.chomp
+  temp_bool = validator.names(name)
+end
 p2 = Players.new(name, ox)
 game = Game.new(p1, p2, grid)
 puts grid.board
 temp_game = game.playing?
 puts "#{p1.name} make your firts move"
 pos = gets.chomp
+validate_input = validator.inputs(pos, grid)
+while  !validate_input
+  puts 'Invalid postion please reenter a correct position'
+  pos = gets.chomp
+  validate_input = validator.inputs(pos, grid)
+end
 who_play = game.who_plays(p2)
 game.movements(p1, pos.to_i)
 while temp_game
-  who_play = turn(who_play,game,grid)
+  who_play = turn(who_play, game, grid, validator)
   temp_game = game.playing?
 end
 
