3a260aec895c7db96e0ac2173697090853977de9
Salvador <salvador.on@outlook.com>
2020-02-24 18:35:11 -0300

    add move, inputs and symbols test

--- spec/testing_spec.rb
+++ spec/testing_spec.rb
@@ -1,10 +1,39 @@
-require '../lib/validation.rb'
+# frozen_string_literal: true
+
+require './lib/validation.rb'
+require './lib/grid.rb'
 
 RSpec.describe Validation do
-  describe "#names" do
-    it "returns true when valid" do
+
+  describe '#move' do
+    it 'returns true if grid value is numeric' do
+      grid = Grid.new
+      grid.grid[0] = 'X'
+      validation = Validation.new
+      expect(validation.move(0, grid)).to eql(false)
+    end
+  end
+
+  describe '#inputs' do
+    it 'returns true if grid value is numeric and positive and lower or iqual than 9' do
+    grid = Grid.new
+    grid.grid[0] = 'X'
+    validation = Validation.new
+    expect(validation.move(0, grid)).to eql(false)
+    end
+  end
+
+  describe '#names' do
+    it 'returns true when valid' do
       validation = Validation.new
       expect(validation.names('sala')).to eql(false)
+    end
+  end
+
+  describe '#symbols' do
+    it 'returns true when valid' do
+      validation = Validation.new
+      expect(validation.symbols('x')).to eql(true)
     end
   end
 end
